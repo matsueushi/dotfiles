3752122bb30a56867f8a2313b57761c28f3caf8b
matsueushi <matsueushi@gmail.com>
2019-10-24 00:12:21 -0400

    add julia

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 Dotfiles
 ========
 
+```shell
+bash -c "$(curl -L raw.githubusercontent.com/matsueushi/dotfiles/master/install.sh)"
+```
--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-echo "Downloading dotfiles..."
+echo "Loading dotfiles..."
+
+sudo apt-get update
+sudo apt-get install julia

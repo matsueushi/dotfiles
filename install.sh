9cacbab2b4e5a6b1684d7a69ace7a0255463d39b
matsueushi <matsueushi@gmail.com>
2019-10-26 04:01:28 +0000

    Commit test, git config

--- install.sh
+++ install.sh
@@ -2,5 +2,12 @@
 
 echo "Loading dotfiles..."
 
+echo "install julia..."
 sudo apt update
 sudo apt -y install julia
+
+echo "set git identity..."
+git config --global user.email matsueushi@gmail.com
+git config --global user.name matsueushi
+
+echo "Finished!"

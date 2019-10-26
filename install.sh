80523bc4b8849a4104f195b3a77f032ec1c27bf4
matsueushi <matsueushi@gmail.com>
2019-10-26 21:56:25 +0000

    remove apt get julia

--- install.sh
+++ install.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 
-echo "Loading dotfiles..."
+echo "loading dotfiles..."
 
-echo "install julia..."
-# git is also intalled
+echo "install git..."
 sudo apt update
-sudo apt -y install julia
+sudo apt -y install git-all
+# git config
+git config --global include.path ~/dotfiles/gitconfig
 
-# clone git repository
+echo "clone git repository..."
 if [ ! -d ~/dotfiles ]; then
     git clone https://github.com/matsueushi/dotfiles.git ~/dotfiles
 fi
 
-# git config
-git config --global include.path ~/dotfiles/gitconfig
+echo "install julia..."
+JULIA_PATH=/usr/local/julia
 
-julia -e 'Pkg.add("Pkg")'
-julia -e 'import Pkg; Pkg.add(["FileIO", "Images", "Flux"])'
+curl -o /tmp/julia.tar.gz "https://julialang-s3.julialang.org/bin/linux/x64/1.2/julia-1.2.0-linux-x86_64.tar.gz"
+mkdir "$JULIA_PATH"
+tar -xzf /tmp/julia.tar.gz -C "$JULIA_PATH"
 
-echo "Finished!"
+# julia -e 'Pkg.add("Pkg")'
+# julia -e 'import Pkg; Pkg.add(["FileIO", "Images", "Flux"])'
+
+echo "finished!"

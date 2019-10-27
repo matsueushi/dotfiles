6195b3c481d2b2ceab518fc041d21ab300c391a1
matsueushi <matsueushi@gmail.com>
2019-10-27 02:31:29 +0000

    Install julia from official binary

--- install.sh
+++ install.sh
@@ -5,7 +5,6 @@ echo "loading dotfiles..."
 echo "install git..."
 sudo apt update
 sudo apt -y install git-all
-# git config
 git config --global include.path ~/dotfiles/gitconfig
 
 echo "clone git repository..."
@@ -15,12 +14,13 @@ fi
 
 echo "install julia..."
 JULIA_PATH=/usr/local/julia
+JULIA_VERSION=1.2.0
 
-curl -o /tmp/julia.tar.gz "https://julialang-s3.julialang.org/bin/linux/x64/1.2/julia-1.2.0-linux-x86_64.tar.gz"
-mkdir "$JULIA_PATH"
-tar -xzf /tmp/julia.tar.gz -C "$JULIA_PATH"
+curl -o /tmp/julia.tar.gz "https://julialang-s3.julialang.org/bin/linux/x64/${JULIA_VERSION:0:3}/julia-${JULIA_VERSION}-linux-x86_64.tar.gz"
+sudo mkdir -p "$JULIA_PATH"
+sudo tar -xzf /tmp/julia.tar.gz -C "$JULIA_PATH"
+sudo ln -s "${JULIA_PATH}/julia-${JULIA_VERSION}/bin/julia" /usr/local/bin/julia
 
-# julia -e 'Pkg.add("Pkg")'
-# julia -e 'import Pkg; Pkg.add(["FileIO", "Images", "Flux"])'
+julia -e 'import Pkg; Pkg.add(["FileIO", "Images", "Flux"])'
 
 echo "finished!"

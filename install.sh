284f48f733050825341aa525d7c2d09619412d48
matsueushi <matsueushi@gmail.com>
2019-11-09 21:15:02 +0000

    docker

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# ubuntu
 
 cd ~
 
@@ -20,5 +21,22 @@ bash ~/dotfiles/install-python.sh
 
 # install julia
 bash ~/dotfiles/install-julia.sh
+
+# install docker
+echo "install docker..."
+sudo apt -y install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt update
+sudo apt -y install docker-ce docker-ce-cli containerd.io
 
 echo "finished!"

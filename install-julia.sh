e648649bba9126e64e7d4efd1af69c2db185105b
matsueushi <matsueushi@gmail.com>
2020-03-21 21:11:02 -0400

    julia 1.4.0

--- install-julia.sh
+++ install-julia.sh
@@ -3,7 +3,7 @@
 echo "install julia..."
 JULIA_PATH=/usr/local/julia
 JULIA_BIN_PATH=/usr/local/bin/julia
-JULIA_VERSION=1.3.0
+JULIA_VERSION=1.4.0
 JULIA_URL=https://julialang-s3.julialang.org/bin/linux/x64/${JULIA_VERSION:0:3}/julia-${JULIA_VERSION}-linux-x86_64.tar.gz
 
 echo "Download from ${JULIA_URL}"
